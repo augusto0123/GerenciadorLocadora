0203a2bb02b801b79ccb1569def435c262f426ee
Augusto <senescal.oliveira@gmail.com>
2025-03-25 22:21:51 -0300

    feat: adicionado função para adicionar filmes na lista

--- GerenciadorLocadora/src/Main.java
+++ GerenciadorLocadora/src/Main.java
@@ -1,5 +1,14 @@
+import model.Filme;
+
 public class Main {
     public static void main(String[] args) {
+
+        Filme filme1 = new Filme("Filme1", "Diretor1", 1950);
+        Filme filme2 = new Filme("Filme2", "Diretor1", 1960);
+        Filme filme3 = new Filme("Filme3", "Diretor2", 2010);
+        Filme filme4 = new Filme("Filme4", "Diretor1", 1990);
+        Filme filme5 = new Filme("Filme5", "Diretor3", 2002);
+        Filme filme6 = new Filme("Filme6", "Diretor3", 2015);
 
     }
 }
