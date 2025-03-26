51c833be6a8457da6314b893ab858fe9671dd12a
Augusto <senescal.oliveira@gmail.com>
2025-03-25 22:27:45 -0300

    fix: ajuste main para chamar adicionar-filme

--- GerenciadorLocadora/src/Main.java
+++ GerenciadorLocadora/src/Main.java
@@ -1,7 +1,13 @@
+import locadora.Locadora;
 import model.Filme;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Locadora locadora = new Locadora();
 
         Filme filme1 = new Filme("Filme1", "Diretor1", 1950);
         Filme filme2 = new Filme("Filme2", "Diretor1", 1960);
@@ -9,6 +15,13 @@ public class Main {
         Filme filme4 = new Filme("Filme4", "Diretor1", 1990);
         Filme filme5 = new Filme("Filme5", "Diretor3", 2002);
         Filme filme6 = new Filme("Filme6", "Diretor3", 2015);
+
+        locadora.adicionarFilme(filme1);
+        locadora.adicionarFilme(filme2);
+        locadora.adicionarFilme(filme3);
+        locadora.adicionarFilme(filme4);
+        locadora.adicionarFilme(filme5);
+        locadora.adicionarFilme(filme6);
 
     }
 }
--- GerenciadorLocadora/src/locadora/Locadora.java
+++ GerenciadorLocadora/src/locadora/Locadora.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 public class Locadora {
 
+    // Criada a lista para adicionar filmes
     List<Filme> filmes = new ArrayList<>();
 
+    // Funcao para adicionar filmes na lista
     public void adicionarFilme(Filme filme){
         filmes.add(filme);
     }
